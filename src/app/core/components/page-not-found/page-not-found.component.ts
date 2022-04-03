6b30a8327881cd04152bcd10072ced417164991e
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2022-04-03 22:55:23 +0200

    feat: add page-not-found component

--- src/app/core/components/page-not-found/page-not-found.component.ts
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+const ICON_NOT_FOUND_PATH = './assets/icons/page-not-found-icon.svg';
 
 @Component({
   selector: 'ytube-client-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
-  ngOnInit(): void {}
+export class PageNotFoundComponent {
+  public readonly notFoundIcon: string = 'not-found';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(this.notFoundIcon, sanitizer.bypassSecurityTrustResourceUrl(ICON_NOT_FOUND_PATH));
+  }
 }
